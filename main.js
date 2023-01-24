74c2472e7d563e5ee8f4a333f4759c204b76be10
carloos-05 <cvcardos5@gmail.com>
2023-01-24 00:19:01 -0300

    sobre mim

--- main.js
+++ main.js
@@ -4,20 +4,13 @@ const myNameElement = $("#my-name");
 myNameElement.innerText = `${getRandomString(6)} ${getRandomString(7)}`
 let letterIndex = 1;
 
-// const writingInterval = setInterval(() => {
-//   setInterval(() => {
-//     myNameElement.innerText = `${getRandomString(6)} ${getRandomString(7)}`
-//   },80)
-//   if (letterIndex > myName.length) return;
-//   myNameElement.innerText = myName.slice(0, letterIndex);
-//   letterIndex++;
-// }, 160);
-
 const writingInterval = setInterval(() => {
     if (letterIndex > myName.length) clearInterval(writingInterval)
-    const myNameString = myName.substring(0, letterIndex)
-    const randomStr = `${getRandomString(6)} ${getRandomString(7)}`.substring(letterIndex, myName.length)
-    myNameElement.innerText = myNameString + randomStr;
+    setInterval(() => {
+      const myNameString = myName.substring(0, letterIndex)
+      const randomStr = `${getRandomString(6)} ${getRandomString(7)}`.substring(letterIndex, myName.length)
+      myNameElement.innerText = myNameString + randomStr;
+    }, 80)
     letterIndex++
 }, 160);
 
